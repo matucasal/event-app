6566067b1975e8b91619f54faeeb4d4e6fa635b7
Matias Casal <matiascasalh@gmail.com>
2019-05-27 17:52:37 -0300

    Subo toda la data del router y vuetify

--- FRONT_END/src/main.js
+++ FRONT_END/src/main.js
@@ -1,14 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
-import Vuetify from 'vuetify'
+import Vuetify from 'vuetify';
+import router from './router.js';
 
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false;
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
+Vue.use(router);
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
 
