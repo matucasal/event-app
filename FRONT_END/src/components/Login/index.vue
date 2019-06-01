e42fda29347cf620d0369935dbb339b84b118e35
cambio <MY_NAME@example.com>
2019-06-01 11:15:45 -0300

    "Subo login"

---
+++ FRONT_END/src/components/Login/index.vue
@@ -0,0 +1,42 @@
+<template>  
+    <div>    
+        <h2>Login</h2>    
+        <form v-on:submit="login">    
+            <input type="text" name="email" /><br>    
+            <input type="password" name="password" /><br>    
+            <input type="submit" value="Login" />    
+        </form>    
+    </div>
+</template>
+
+<script>  
+    import router from "../../router.js"    
+    import axios from "axios"    
+    export default {    
+        name: "Login",    
+        methods: {    
+            login: (e) => {    
+                e.preventDefault()    
+                let email = "user@email.com"   
+                let password = "password"    
+                let login = () => {    
+                    let data = {    
+                        email: email,    
+                        password: password    
+                    }    
+                    axios.post("/api/login", data)    
+                        .then((response) => {    
+                            console.log("Logged in")    
+                            //si sale bien lo mango al dashboard
+                            router.push("/layout")    
+                        })    
+                        .catch((errors) => {    
+                            console.log("Cannot log in")    
+                        })    
+                }    
+                login()    
+            }    
+        }    
+    }
+</script>
+
